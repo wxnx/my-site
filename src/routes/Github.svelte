f6149cf0e02b796d041d09203f5b55a35ce7d240
wxnx <wino99oniw@gmail.com>
2022-03-13 10:37:18 +0700

    modified:   src/routes/Github.svelte

--- src/routes/Github.svelte
+++ src/routes/Github.svelte
@@ -1,10 +1,14 @@
 <script>
-    let Stats = 'https://github-readme-stats.vercel.app/api?username=wxnx&show_icons=true&theme=radical';
+    let Stats = 'https://github-readme-stats.vercel.app/api?username=wxnx&show_icons=true&theme=dark&custom_title=Github%20Stats&layout=compact&count_private=true';
+    let Used_Language = 'https://github-readme-stats.vercel.app/api/top-langs/?username=wxnx&langs_count=100&layout=compact&theme=dark&custom_title=Most%20Used%20Languages&card_width=444&count_private=true';
+    let Coding_Stats = 'https://github-readme-stats.vercel.app/api/wakatime?username=wxnx&theme=dark&custom_title=Weekly%20Coding%20Stats&card_width=440';
 </script>
 <main>
     <h2>
         Github
     </h2>
     <img src={Stats} alt="">
+    <img src={Used_Language} alt="">
+    <img src={Coding_Stats} alt="">
 </main>
 
