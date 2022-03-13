5d2a4102b3ff97d3cada8daf2b32a7d7595b824e
wxnx <wino99oniw@gmail.com>
2022-03-13 09:46:14 +0700

    modified:   src/routes/Github.svelte

--- src/routes/Github.svelte
+++ src/routes/Github.svelte
@@ -1,6 +1,9 @@
+<script>
+    let Stats = '<a href="https://github-readme-stats.vercel.app/api?username=wxnx&show_icons=true&theme=dark&layout=compact&count_private=true" >';
+</script>
 <main>
     <h2>
         Github
     </h2>
-    ![Github Stats](https://github-readme-stats.vercel.app/api?username=wxnx&show_icons=true&theme=dark&layout=compact&count_private=true)
+    <img src={Stats} alt="">
 </main>
