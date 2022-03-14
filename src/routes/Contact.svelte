a06ed9ef14bf3fc299a05b51e6b4b144cc6fdea6
wxnx <wino99oniw@gmail.com>
2022-03-14 11:55:53 +0700

    modified:   src/routes/Contact.svelte 	modified:   src/routes/Github.svelte

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -4,3 +4,9 @@
     </h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque commodi beatae repudiandae! Nulla dolor esse vel quos? Mollitia sint repellendus corporis, iusto vel voluptatem, totam distinctio odio neque et explicabo!</p>
 </main>
+
+<style>
+    main{
+        text-align: center;
+    }
+</style>
--- src/routes/Github.svelte
+++ src/routes/Github.svelte
@@ -1,8 +1,9 @@
 <script>
     let Stats = 'https://github-readme-stats.vercel.app/api?username=wxnx&show_icons=true&theme=dark&custom_title=Github%20Stats&layout=compact&count_private=true';
-    let Used_Language = 'https://github-readme-stats.vercel.app/api/top-langs/?username=wxnx&langs_count=100&layout=compact&theme=dark&custom_title=Most%20Used%20Languages&card_width=444&count_private=true';
-    let Coding_Stats = 'https://github-readme-stats.vercel.app/api/wakatime?username=wxnx&theme=dark&custom_title=Weekly%20Coding%20Stats&card_width=440';
+    let Used_Language = 'https://github-readme-stats.vercel.app/api/top-langs/?username=wxnx&langs_count=100&layout=compact&theme=dark&custom_title=Most%20Used%20Languages&count_private=true';
+    let Coding_Stats = 'https://github-readme-stats.vercel.app/api/wakatime?username=wxnx&theme=dark&custom_title=Weekly%20Coding%20Stats';
 </script>
+
 <main>
     <h2>
         Github
@@ -16,4 +17,10 @@
         <img src="https://github-readme-stats.vercel.app/api/pin/?username=wxnx&repo=my-site&theme=dark" alt="" />
     </a>
 </main>
+
+<style>
+    main{
+        text-align: center;
+    }
+</style>
 
