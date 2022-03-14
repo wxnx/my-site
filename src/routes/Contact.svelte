99238e52b2b1446a33825a26d235d48f029548e5
wxnx <wino99oniw@gmail.com>
2022-03-14 12:54:36 +0700

    modified:   src/routes/Contact.svelte

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,8 +1,16 @@
+<script>
+    let linkedin = 'https://img.shields.io/badge/-Linkedin-black?style=for-the-badge&logo=Linkedin';
+    let instagram = 'https://img.shields.io/badge/-Instagram-black?style=for-the-badge&logo=Instagram';
+    let email = 'https://img.shields.io/badge/-Gmail-black?style=for-the-badge&logo=Gmail';
+</script>
+
 <main>
     <h2>
         Contact
     </h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque commodi beatae repudiandae! Nulla dolor esse vel quos? Mollitia sint repellendus corporis, iusto vel voluptatem, totam distinctio odio neque et explicabo!</p>
+    <a href="https://www.linkedin.com/in/winoramaputra/"><img src={linkedin} alt=""></a>
+    <a href="https://www.instagram.com/winoramaputra"><img src={instagram} alt=""></a>
+    <a href="mailto:wino99oniw@gmail.com"><img src={email} alt=""></a>  
 </main>
 
 <style>
